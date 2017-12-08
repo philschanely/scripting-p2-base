628fa74fe3ce1b89ae572be3b4ee8cce45b57d1a
Phil Schanely <pschanely@miletwo.us>
2017-12-07 22:44:00 -0500

    More patches to auth and user endpoints

--- api/application/controllers/User.php
+++ api/application/controllers/User.php
@@ -12,20 +12,14 @@ class User extends CI_Controller {
     public function index($_key='all')
     {
         $result = $this->user_model->route($_key);
-        $auth = (object) array(
-            'authenticated' => false,
-            'user' => NULL
-        );
-        if (isset($result->users) && !empty($result->users)) {
-            $auth->authenticated = true;
-            $auth->user = $result->users[0];
-            unset($auth->user->password);
-        } elseif (is_object($result) && !empty($result->id)) {
-            $auth->authenticated = true;
-            $auth->user = $result;
-            unset($auth->user->password);
+        if (is_array($result)) {
+            foreach ($result as &$user) {
+                unset($user->password);
+            }
+        } elseif (is_object($result)) {
+            unset($result->password);
         }
-        echo json_encode($auth);
+        echo json_encode($result);
     }
 
 }
