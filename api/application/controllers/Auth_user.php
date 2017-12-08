fd15c3e7bb6407009085ddeba86cc2fa56bf36ae
Phil Schanely <pschanely@miletwo.us>
2017-12-08 07:27:00 -0500

    Sync with student base for cleaner start

--- api/application/controllers/Auth_user.php
+++ api/application/controllers/Auth_user.php
@@ -11,14 +11,18 @@ class Auth_User extends CI_Controller {
 
     public function index($_key='all')
     {
+        $result = $this->user_model->route($_key);
         $auth = (object) array(
             'authenticated' => false,
             'user' => NULL
         );
-        $result = $this->user_model->route($_key);
-        if (count($result->users) > 0) {
+        if (isset($result->users) && !empty($result->users)) {
             $auth->authenticated = true;
             $auth->user = $result->users[0];
+            unset($auth->user->password);
+        } elseif (is_object($result) && !empty($result->id)) {
+            $auth->authenticated = true;
+            $auth->user = $result;
             unset($auth->user->password);
         }
         echo json_encode($auth);
